987eb9cae1e57ddcb2cdaefd0c5daaf6ffd4cfdc
@kisrakap <kisrakap@gmail.com>
2020-06-06 18:46:23 +0800

    replace all files

--- dom.js
+++ dom.js
@@ -1,76 +1,84 @@
-let moves = 0;
-let counter = document.querySelector(".moves");
-var button = document.getElementsByClassName("button")
-let array = [...button];
+
+var memoryArray = ['K','I','S','R','A','3','K','I','S','R','A','3']
+ var memoryValue = []
+ var memorytile = []
+ var tilefliped = 0
+
+    Array.prototype.memorytileshuffle = function(){
+        var i = this.length, j, temp; 
+    while(--i > 0){
+         j = Math.floor(Math.random() * (i + 1)); 
+        temp = this[j]; 
+        this[j] = this[i] ;
+        this[i] = temp;
+                }
+    } 
+
+ function newboard(){
+     tilefliped = 0;
+     var output = '';
+     memoryArray.memorytileshuffle();
+     for(var i = 0 ; i < memoryArray.length ; i++){
+         output += '<div id = "tile_'+i+'" onclick="memoryfliptile(this,\''+memoryArray[i]+'\')"></div>';
+     }
+     document.getElementById('memory_board').innerHTML = output;
+     setTimeout(newboard, 10000)
+    
+    }
 
 
-    // document.body.onload = init();
-        function kliktombol(x){
-            document.getElementById("btn"+x+"D").style.transform="perspective(600px)rotateY(-180deg)";
-            document.getElementById("btn"+x+"B").style.transform="perspective(600px)rotateY(0deg)";            
-            // reset moves
-             moves++;
-             counter.innerHTML = moves;
 
+ function memoryfliptile(tile, val){
+     if (tile.innerHTML == "" && memoryValue.length < 2){
+         tile.style.background = 'coral';
+         tile.innerHTML = val ;
+         if (memoryValue.length == 0){
+             memoryValue.push(val) ; 
+             memorytile.push(tile.id)
+         }
+         else if (memoryValue.length == 1){
+             memoryValue.push(val);
+             memorytile.push(tile.id);
 
-            if (moves == 2){
-                match()
-            }
+             if(memoryValue[0] == memoryValue[1]){
+                 tilefliped+=2 ;
 
-        }   
-                moves = 0
-        function match() {
-            
-              if (document.getElementById("btn"+x+"B").src == document.getElementById("btn"+x+1+"B")){
-                document.getElementById("btn"+x+"B").disabled="disabled";
-              }
-              else {
-                document.getElementById("btn"+x+"D").style.transform="perspective(600px)rotateY(-180deg)";
-              }
-              
-        }
+                 // clear array
+                 memoryValue = []
+                 memorytile = []
 
-        
+                 // cek jika semua board cleared 
+                 if (tilefliped == memoryArray.length){
+                    alert("Selamat Kamu berhasil");
+                    document.getElementById('memory_board').innerHTML = newboard();
+                 }
+                 else {
+                    alert("Kamu kalah. lagi ?");
+                    document.getElementById('memory_board').innerHTML = newboard();
+                 }
+             }
 
-        function menang(){
-            alert("kamu menang")
-        }
+             else {
+                 function flip2back(){
 
-        function kalah(){
-            alert("kamu kalah")
-        }
-        
-       
-        
-//         //Count player's moves
-// function moveCounter(){
-//     moves++;
-//     counter.innerHTML = moves;
-//     //start timer on first click
-//     if(moves == 1){
-//         second = 0;
-//         minute = 0; 
-//         hour = 0;
-//         startTimer();
-//     }
-// //Timer
-// var second = 0, minute = 0; hour = 0;
-// var timer = document.querySelector(".timer");
-// var interval;
-// function startTimer(){
-//     interval = setInterval(function(){
-//         timer.innerHTML = " " + minute+" mins "+second+" secs";
-//         second++;
-//         if(second == 60){
-//             minute++;
-//             second=0;
-//         }
-//         if(minute == 60){
-//             hour++;
-//             minute = 0;
-//         }
-//     },1000);
-// }
+                     var tile_1 = document.getElementById(memorytile[0]);
+                     var tile_2 = document.getElementById(memorytile[1]);
+                     
+                     tile_1.style.background = 'url(tile_bg.jpg no-repeat ';
+                     tile_1.innerHTML = ''
+                     tile_2.style.background = 'url(tile_bg.jpg no-repeat ';
+                     tile_2.innerHTML = ''
+
+                     memoryValue = []
+                     memorytile = []
+                 }
+                  setTimeout(flip2back, 800)   
+                 }
+             }
+         }
+     }
+ 
+
 
 
        
